490b478dff9e9f5bfd51b57b47591b8350f016f4
me <juniorjr083@gmail.com>
2019-02-09 20:43:33 -0500

    setting the state for the register component

--- auth-boiler/src/components/Login.js
+++ auth-boiler/src/components/Login.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-
-
-
+ 
 class Login extends Component {
   render() {
     return (
       
       <div className="App">
       <h1> LOGIN to your account </h1>
-      <form className="form"> 
+      <form  className="form"> 
           
-          Email <input className="form-control" type="text" id="email" name="email"/>
+          Email <input  className="form-control" type="text" id="email" name="email"/>
           Password <input className="form-control" type="text" id="password" name="password"/>
 
           </form>
--- auth-boiler/src/components/Register.js
+++ auth-boiler/src/components/Register.js
@@ -4,6 +4,58 @@ import '../css/register.css';
 
 
 class Register extends Component {
+
+  state = {
+    name: '',
+    email: '',
+    password: ''
+   
+  }
+ 
+ 
+ handleChange = (e) => {
+ 
+   
+ 
+   this.setState({
+     [e.target.id]: e.target.value
+   }, () => {console.log(this.state)})
+ 
+ }
+ 
+ handleSubmit = (e) =>{
+   e.preventDefault();
+   let name= this.state.name;
+   let email = this.state.email;
+   let password = this.state.password;
+ 
+   fetch('http://localhost:3000/api/v1/users', {
+     method: 'POST',
+     headers: {
+       'Accepts': 'application/json',
+       'Content-Type':'application/json'
+     },
+     body: JSON.stringify({
+       name: this.state.name,
+       email: this.state.email,
+       password: this.state.password
+     })
+ 
+     
+   })
+   .then(res => res.json())
+   .then(data => console.log(`this is the data from the server ${data}`))
+ 
+ }
+ 
+
+
+
+
+
+
+
+
   render() {
     return (
         <center >
@@ -11,10 +63,10 @@ class Register extends Component {
     
       <h1>Register</h1>
 
-      <form className="form"> 
-          Name <input className="form-control" type="text" id="name" name="name"/> <br/>
-          Email <input className="form-control" type="text" id="email" name="email"/> <br/>
-          Password <input className="form-control" type="text" id="password" name="password"/><br/> <br/> <br/> <br/>
+      <form  onSubmit={this.handleSubmit} className="form"> 
+          Name <input onChange={this.handleChange} className="form-control" type="text" id="name" name="name"/> <br/>
+          Email <input onChange={this.handleChange} className="form-control" type="text" id="email" name="email"/> <br/>
+          Password <input onChange={this.handleChange} className="form-control" type="text" id="password" name="password"/><br/> <br/> <br/> <br/>
             <input type="submit" value="Submit" className="form-control btn btn-success"/>
           </form>
 
